60412fa7a7e0e9c0cb219e9a3f886a54423e41eb
abdul ansari <samunleash1@yahoo.com>
2024-08-24 01:41:40 +0400

    refactor for aggregate method

--- go.mod
+++ go.mod
@@ -15,9 +15,14 @@ require (
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/rs/zerolog v1.33.0 // indirect
 	golang.org/x/crypto v0.26.0 // indirect
+	golang.org/x/net v0.26.0 // indirect
 	golang.org/x/sync v0.8.0 // indirect
 	golang.org/x/sys v0.24.0 // indirect
 	golang.org/x/text v0.17.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240604185151-ef581f913117 // indirect
+	google.golang.org/grpc v1.65.0 // indirect
+	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.5.1 // indirect
+	google.golang.org/protobuf v1.34.2 // indirect
 	gorm.io/driver/postgres v1.5.9 // indirect
 	gorm.io/gorm v1.25.11 // indirect
 )
--- main.go
+++ main.go
@@ -1,78 +1,19 @@
 package main
 
 import (
-	"encoding/json"
-	"time"
-
 	"github.com/abdulshakoor02/ohlc_exinity/config"
 	"github.com/abdulshakoor02/ohlc_exinity/database/dbAdapter"
 	"github.com/abdulshakoor02/ohlc_exinity/database/migration"
-	"github.com/abdulshakoor02/ohlc_exinity/database/operation"
-	"github.com/abdulshakoor02/ohlc_exinity/logger"
-	"github.com/abdulshakoor02/ohlc_exinity/models/ohlcRecord"
-	"github.com/abdulshakoor02/ohlc_exinity/models/trade"
-	"github.com/gorilla/websocket"
+	"github.com/abdulshakoor02/ohlc_exinity/service/aggregateData"
 )
 
 func main() {
-	log := logger.Logger
 	config.LoadEnv()
 	dbAdapter.DbConnect()
 	migration.MigrateDb()
 
-	url := "wss://stream.binance.com:9443/ws/btcusdt@aggTrade"
+	go aggregateData.AggregateData("ethusdt", true)
+	go aggregateData.AggregateData("pepeusdt", true)
+	aggregateData.AggregateData("btcusdt", true)
 
-	conn, _, err := websocket.DefaultDialer.Dial(url, nil)
-	if err != nil {
-		log.Fatal().Err(err).Msgf("failed to load env")
-	}
-	defer conn.Close()
-	var currentOHLC *ohlcRecord.OhlcRecord
-	var currentMinute time.Time
-
-	for {
-		var trade trade.Trade
-		_, message, err := conn.ReadMessage()
-		if err != nil {
-			log.Println("Error reading message:", err)
-			return
-		}
-		if err := json.Unmarshal(message, &trade); err != nil {
-			log.Println("Error unmarshaling JSON:", err)
-			continue
-		}
-		tradeTime := time.Unix(0, trade.EventTime*int64(time.Millisecond))
-		minuteStart := tradeTime.Truncate(time.Minute)
-
-		if currentOHLC == nil || minuteStart != currentMinute {
-			if currentOHLC != nil {
-				go operation.Create[ohlcRecord.OhlcRecord](currentOHLC)
-			}
-			currentOHLC = &ohlcRecord.OhlcRecord{
-				TradingPair: trade.TradingPair,
-				OpenTime:    minuteStart,
-				CloseTime:   minuteStart.Add(time.Minute - time.Nanosecond),
-				Open:        trade.Price,
-				High:        trade.Price,
-				Low:         trade.Price,
-				Close:       trade.Price,
-			}
-			currentMinute = minuteStart
-		} else {
-			currentOHLC.Close = trade.Price
-			if trade.Price > currentOHLC.High {
-				currentOHLC.High = trade.Price
-			}
-			if trade.Price < currentOHLC.Low {
-				currentOHLC.Low = trade.Price
-			}
-		}
-
-		// finalVal, err := json.Marshal(currentOHLC)
-		// if err != nil {
-		// 	fmt.Println("Error:", err)
-		// }
-		//
-		// fmt.Printf("Received message: %s\n", finalVal)
-	}
 }
